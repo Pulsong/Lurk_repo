a204bdb5946a30d1fbf61618aa8e89755e97ee77
Asko Salmenaho <asko.salmenaho@vuoksi.fi>
2021-09-10 17:25:26 +0300

    Feature playground with movement
    Tuplahyppy, seinäjuoksu, dash ja orientaatio seuraa hahmoa, mutta ei toimi, jos rotatee koko pelaajaa.
    
    Lisätty myös vähän testi maastoa

---
+++ Projekti_Lurk/Assets/Scriptit/PlayerMovement.cs
@@ -0,0 +1,390 @@
+
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Wallrun scroll down for movement 
+
+    public LayerMask whatIsWall;
+    public float wallrunForce, maxWallrunTime,maxWallSpeed;
+    bool isWallRight, isWallLeft;
+    bool isWallRunning;
+    public float maxWallRunCameraTilt, WallRunCameraTilt;
+
+    private void WallRunInput()
+    {
+        //Start Wallrun
+        if (Input.GetKey(KeyCode.D) && isWallRight) StartWallRun();
+        if (Input.GetKey(KeyCode.A) && isWallLeft) StartWallRun();
+    }
+
+    private void StartWallRun()
+    {
+        rb.useGravity = false;
+        isWallRunning = true;
+
+        if (rb.velocity.magnitude <= maxWallSpeed)
+        {
+            rb.AddForce(orientation.forward * wallrunForce * Time.deltaTime);
+
+            //Make sure Character sticks to the wall
+            if (isWallRight)
+            {
+                rb.AddForce(orientation.right * wallrunForce / 5 * Time.deltaTime);
+            }
+            else
+            {
+                rb.AddForce(-orientation.right * wallrunForce / 5 * Time.deltaTime);
+            }
+        }
+    }
+    private void StopWallRun()
+    {
+        rb.useGravity = true;
+        isWallRunning = false;
+    }
+    private void CheckForWall()
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.right, 2f, whatIsWall);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, 2f, whatIsWall);
+
+        // leave wallrun 
+        if (!isWallLeft && !isWallRight) StopWallRun();
+        //reset double jump (jos sellanen halutaan)
+        if (isWallLeft || isWallRight) doubleJumpsLeft = startDoubleJumps;
+
+
+
+
+
+
+    }
+
+    
+    //Assingables
+    public Transform playerCam;
+    public Transform orientation;
+
+    //Other
+    private Rigidbody rb;
+
+    //Rotation and look
+    private float xRotation;
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
+
+    //Movement
+    public float moveSpeed = 4500;
+    public float maxSpeed = 20;
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    public float counterMovement = 0.175f;
+    private float threshold = 0.01f;
+    public float maxSlopeAngle = 35f;
+
+
+
+    //Jumping
+    private bool readyToJump = true;
+    private float jumpCooldown = 0.25f;
+    public float jumpForce = 550f;
+
+    public int startDoubleJumps = 1;
+    int doubleJumpsLeft;
+    private Vector3 normalVector = Vector3.up;
+
+    //Input
+    float x, y;
+    bool jumping, sprinting;
+
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Update()
+    {
+        MyInput();
+        CheckForWall();
+        WallRunInput();
+        Look();
+    }
+
+    /// <summary>
+    /// Find user input. Should put this in its own class but im lazy
+    /// </summary>
+    private void MyInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        jumping = Input.GetButton("Jump");
+
+
+        //Double Jumping
+        if (Input.GetButtonDown("Jump") && !grounded && doubleJumpsLeft >= 1)
+        {
+            Jump();
+            doubleJumpsLeft--;
+        }
+    }
+
+
+
+
+
+    private void Movement()
+    {
+        //Extra gravity
+        rb.AddForce(Vector3.down * Time.deltaTime * 10);
+
+        //Find actual velocity relative to where player is looking
+        Vector2 mag = FindVelRelativeToLook();
+        float xMag = mag.x, yMag = mag.y;
+
+        //Counteract sliding and sloppy movement
+        CounterMovement(x, y, mag);
+
+        //If holding jump && ready to jump, then jump
+        if (readyToJump && jumping) Jump();
+
+        //Set max speed
+        float maxSpeed = this.maxSpeed;
+
+
+
+        //If speed is larger than maxspeed, cancel out the input so you don't go over max speed
+        if (x > 0 && xMag > maxSpeed) x = 0;
+        if (x < 0 && xMag < -maxSpeed) x = 0;
+        if (y > 0 && yMag > maxSpeed) y = 0;
+        if (y < 0 && yMag < -maxSpeed) y = 0;
+
+        //Some multipliers
+        float multiplier = 1f, multiplierV = 1f;
+
+
+        // Movement in air
+        if (!grounded)
+        {
+            multiplier = 0.5f;
+            multiplierV = 0.5f;
+        }
+
+
+        //Apply forces to move player
+        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
+        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+    }
+
+    private void Jump()
+    {
+        if (grounded && readyToJump)
+        {
+            readyToJump = false;
+
+            //Add jump forces
+            rb.AddForce(Vector2.up * jumpForce * 1.5f);
+            rb.AddForce(normalVector * jumpForce * 0.5f);
+
+            doubleJumpsLeft = startDoubleJumps;
+
+
+            //If jumping while falling, reset y velocity.
+            Vector3 vel = rb.velocity;
+            if (rb.velocity.y < 0.5f)
+                rb.velocity = new Vector3(vel.x, 0, vel.z);
+            else if (rb.velocity.y > 0)
+                rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+
+
+        }
+        if (!grounded)
+        {
+            readyToJump = false;
+
+            //Add jump forces
+            rb.AddForce(orientation.forward * jumpForce * 1f);
+            rb.AddForce(Vector2.up * jumpForce * 1.5f);
+            rb.AddForce(normalVector * jumpForce * 0.5f);
+
+            //Reset Velocity
+            rb.velocity = Vector3.zero;
+
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+        //Walljump
+        if (isWallRunning)
+        {
+            readyToJump = false;
+
+            //normal jump
+            if (isWallLeft && !Input.GetKey(KeyCode.D) || isWallRight && !Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(Vector2.up * jumpForce * 1.5f);
+                rb.AddForce(normalVector * jumpForce * 0.5f);
+            }
+
+            //sidwards wallhop
+            if (isWallRight || isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) rb.AddForce(-orientation.up * jumpForce * 1f);
+            if (isWallRight && Input.GetKey(KeyCode.A)) rb.AddForce(-orientation.right * jumpForce * 3.2f);
+            if (isWallLeft && Input.GetKey(KeyCode.D)) rb.AddForce(orientation.right * jumpForce * 3.2f);
+
+            //Always add forward force
+            rb.AddForce(orientation.forward * jumpForce * 1f);
+
+        }
+    }
+
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private float desiredX;
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        //Find current look rotation
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+
+        //Rotate, and also make sure we dont over- or under-rotate.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Perform the rotations
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, WallRunCameraTilt);
+        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
+
+        //While wallrunning 
+        //Tilts the camera in 0.5 second
+
+        if (Math.Abs(WallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallRight)
+        {
+            WallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;
+        }
+        if (Math.Abs(WallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallLeft)
+        {
+            WallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+        }
+        //Tilts camera back again
+        if (WallRunCameraTilt > 0 && !isWallRight && !isWallLeft)
+            WallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+        if (WallRunCameraTilt < 0 && !isWallRight && !isWallLeft)
+            WallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;
+
+    }
+
+    private void CounterMovement(float x, float y, Vector2 mag)
+    {
+        if (!grounded || jumping) return;
+
+
+
+        //Counter movement
+        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
+        {
+            rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
+        }
+        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
+        {
+            rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
+        }
+
+        //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
+        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
+        {
+            float fallspeed = rb.velocity.y;
+            Vector3 n = rb.velocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(n.x, fallspeed, n.z);
+        }
+    }
+
+    /// <summary>
+    /// Find the velocity relative to where the player is looking
+    /// Useful for vectors calculations regarding movement and limiting movement
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 FindVelRelativeToLook()
+    {
+        float lookAngle = orientation.transform.eulerAngles.y;
+        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+
+        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
+        float v = 90 - u;
+
+        float magnitue = rb.velocity.magnitude;
+        float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
+        float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
+
+        return new Vector2(xMag, yMag);
+    }
+
+    private bool IsFloor(Vector3 v)
+    {
+        float angle = Vector3.Angle(Vector3.up, v);
+        return angle < maxSlopeAngle;
+    }
+
+    private bool cancellingGrounded;
+
+    /// <summary>
+    /// Handle ground detection
+    /// </summary>
+    private void OnCollisionStay(Collision other)
+    {
+        //Make sure we are only checking for walkable layers
+        int layer = other.gameObject.layer;
+        if (whatIsGround != (whatIsGround | (1 << layer))) return;
+
+        //Iterate through every collision in a physics update
+        for (int i = 0; i < other.contactCount; i++)
+        {
+            Vector3 normal = other.contacts[i].normal;
+            //FLOOR
+            if (IsFloor(normal))
+            {
+                grounded = true;
+                cancellingGrounded = false;
+
+                CancelInvoke(nameof(StopGrounded));
+            }
+        }
+
+        //Invoke ground/wall cancel, since we can't check normals with CollisionExit
+        float delay = 3f;
+        if (!cancellingGrounded)
+        {
+            cancellingGrounded = true;
+            Invoke(nameof(StopGrounded), Time.deltaTime * delay);
+        }
+    }
+
+    private void StopGrounded()
+    {
+        grounded = false;
+    }
+
+}
